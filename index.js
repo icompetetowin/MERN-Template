e6a71f76b8d527cdc9ce2869eb59234bfa004f5e
icompetetowin <79755401+icompetetowin@users.noreply.github.com>
2021-07-16 10:38:51 +0000

    Pug, packages, and speed of prototyping!

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express();
 
 // mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
